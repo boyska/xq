5e5f1ed1a5e4b99de5865646b5ddc1928074becd
boyska <piuttosto@logorroici.org>
2020-01-30 17:33:56 +0100

    add --raw: one line per match

--- xq/__main__.py
+++ xq/__main__.py
@@ -12,14 +12,29 @@ from pygments.lexers.html import XmlLexer
 from . import NAME, DESCRIPTION, VERSION
 
 
-def wrap_in_results(elements: [Union[etree._Element, etree._ElementUnicodeResult]]) -> etree._Element:
+def wrap_in_results(
+    elements: [Union[etree._Element, etree._ElementUnicodeResult]]
+) -> etree._Element:
     results = E.results()
     for el in elements:
         results.append(E.result(el))
     return results
 
 
-def apply_xpath(infile, xpath_query=None, colorize=False):
+def raw_out(
+    elements: [Union[etree._Element, etree._ElementUnicodeResult]]
+) -> etree._Element:
+    def tostr(e):
+        if type(e) is etree._ElementUnicodeResult:
+            return str(e)
+        if type(e) is etree._Element:
+            return '[Element <%s>]' % e.tag
+        raise ValueError('This should never happen: unsupported type of match')
+    results = [tostr(e) for e in elements]
+    return results
+
+
+def apply_xpath(infile, xpath_query=None, colorize=False, raw=False):
     try:
         parsed = etree.parse(infile, etree.XMLParser(remove_blank_text=True))
     except etree.XMLSyntaxError:
@@ -27,8 +42,11 @@ def apply_xpath(infile, xpath_query=None, colorize=False):
 
     if xpath_query:
         matches = parsed.xpath(xpath_query)
-        results = wrap_in_results(matches)
-        output = etree.tostring(results, pretty_print=True)
+        if not raw:
+            results = wrap_in_results(matches)
+            output = etree.tostring(results, pretty_print=True)
+        else:
+            output = "\n".join(raw_out(matches))
     else:
         output = etree.tostring(parsed, pretty_print=True)
 
@@ -37,22 +55,31 @@ def apply_xpath(infile, xpath_query=None, colorize=False):
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog=NAME,
-        description=DESCRIPTION,
-    )
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + VERSION)
+    parser = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION)
     parser.add_argument(
-        'xpath_query', nargs='?', type=str,
-        help='XPath query to apply to XML document.'
+        "-v", "--version", action="version", version="%(prog)s " + VERSION
+    )
+    parser.add_argument("-r", "--raw", help="Output plaintext, not html. Only makes sense if your xpath query gives text elements", action="store_true", default=False)
+    parser.add_argument(
+        "xpath_query", nargs="?", type=str, help="XPath query to apply to XML document."
     )
     parser.add_argument(
-        'file', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
-        help='XML file to process. Defaults to STDIN.',
+        "file",
+        nargs="?",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="XML file to process. Defaults to STDIN.",
     )
     args = parser.parse_args()
-    sys.stdout.write(apply_xpath(args.file, args.xpath_query, sys.stdout.isatty()))
+    sys.stdout.write(
+        apply_xpath(
+            args.file,
+            xpath_query=args.xpath_query,
+            colorize=sys.stdout.isatty(),
+            raw=args.raw,
+        )
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
